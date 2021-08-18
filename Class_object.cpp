fd87cfd8b282e182a84fbcd2ecc5fa57f1784d07
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-08-18 22:15:55 +0530

    Create Class_object.cpp

---
+++ Class_object.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class student{
+    public:
+    string name;
+    int age;
+    bool gender;
+
+    void printfn(){
+        cout<<"enter name:"<<endl;
+        cout<<name;
+        cout<<"enter age:"<<endl;
+        cout<<age;
+        cout<<"enter gender:"<<endl;
+        cout<<gender;
+    }
+};
+
+int main()
+{
+    student s[3];
+    for(int i=0;i<3;i++){
+            cout<<"enter name:"<<endl;
+            cin>>s[i].name;
+     cout<<"enter age:"<<endl;
+            cin>>s[i].age;
+             cout<<"enter gender:"<<endl;
+            cin>>s[i].gender;
+    }
+    for(int i=0;i<3;i++){
+        s[i].printfn();
+    }
+
+    return 0;
+}
